b56005833bb7506a4bbdc5e2cca7e6bd36f238b1
miyoda-kar <ritwik18306@iiitd.ac.in>
2019-11-03 00:28:13 +0530

    lawn plus trying animation

---
+++ sample/Controller_gameplay.java
@@ -0,0 +1,43 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+import javafx.animation.*;
+import javafx.scene.shape.*;
+import java.awt.*;
+import javafx.*;
+import javafx.fxml.*;
+import javafx.util.Duration;
+
+
+public class Controller_gameplay implements Initializable {
+
+    @FXML
+    private Button startbutton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+
+    Line path = new Line();
+    Random randoms = new Random();
+    double randx = 50 + (450 - 50) * randoms.nextDouble();
+    path.setStartX(randx);
+    path.setStartY(10);
+    path.setStartX(randx);
+    path.setEndY(375);
+
+    PathTransition fall = new PathTransition();
+    fall.setDuration(Duration.seconds(3));
+   // fall.setNode(startbutton);
+        fall.setPath(path);
+        fall.setCycleCount(PathTransition.INDEFINITE);
+        fall.play();
+}
+
+
+}
+
